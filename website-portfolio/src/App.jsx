12af952e1c638a87b9d33bb20a8d87c141217730
mrfatihrabbani <mr.fatihrabbani@gmail.com>
2024-11-21 21:00:29 +0700

    First Base

--- website-portfolio/src/App.jsx
+++ website-portfolio/src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
-
+    return(
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App
---
+++ website-portfolio/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; Mochammad Rafly Fatih Rabbani - Tekkom KKI {new Date().getFullYear()}</p>
+        </footer>
+    );
+}
+
+export default Footer
